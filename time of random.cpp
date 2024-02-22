1714e2591da16e715ea0ed144bd3d8af610638a9
Popugairulat <krolikitravina@yandex.ru>
2024-02-22 23:58:25 +0300

    время за которое выполняется 1000000 раз функция rand()rand()%N

--- time of random.cpp
+++ time of random.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <chrono>
-const int N = 1000000;
+const int N = 100;
 
 int main() {
 	for (int i = 0; i < 20; ++i) {
 		auto begin = std::chrono::steady_clock::now();
 		for (unsigned cnt = 1000000; cnt != 0; --cnt)
-			int a = rand();
+			int a = rand() * rand() % N;
 		auto end = std::chrono::steady_clock::now();
 		auto time_span =
 			std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
